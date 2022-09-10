64cfba9bd1d80bbe0465908f1f8ea23d2fd7c294
Gideon Nimoh <gnimoh001@gmail.com>
2022-09-10 22:18:22 +0200

    Added the theme switcher to the combined-features project

--- FRONTEND/combined-features/src/js/index.js
+++ FRONTEND/combined-features/src/js/index.js
@@ -3,7 +3,7 @@ import trapFocus from './inert.js';
 const switcher = document.querySelector('#theme-switcher');
 const doc = document.firstElementChild;
 
-switcher.addEventListener('input', (e) => setTheme(e.target.value));
+switcher?.addEventListener('input', (e) => setTheme(e.target.value));
 
 const setTheme = (theme) => doc.setAttribute('color-scheme', theme);
 
