e84afa443818096a040b8bac539b189e50d55545
Gideon Nimoh <gnimoh001@gmail.com>
2022-09-10 12:18:36 +0200

    Added a folder which puts together all the separate features into one single website/web-design which can just be used as a foundation for any static site

---
+++ FRONTEND/combined-features/public/js/index.js
@@ -0,0 +1,32 @@
+import trapFocus from './inert.js';
+
+const switcher = document.querySelector('#theme-switcher');
+const doc = document.firstElementChild;
+
+switcher.addEventListener('input', (e) => setTheme(e.target.value));
+
+const setTheme = (theme) => doc.setAttribute('color-scheme', theme);
+
+const nav = document.querySelector('#mainnav');
+const list = nav.querySelector('ul');
+const burgerClone = document
+	.querySelector('#burger-template')
+	.content.cloneNode(true);
+const button = burgerClone.querySelector('button');
+
+button.addEventListener('click', () => {
+	const isOpen = button.getAttribute('aria-expanded') === 'false';
+	button.setAttribute('aria-expanded', isOpen);
+});
+
+nav.addEventListener('keydown', (event) => {
+	if (event.code === 'Escape') {
+		button.setAttribute('aria-expanded', false);
+	}
+});
+
+nav.insertBefore(burgerClone, list);
+
+button.addEventListener('click', () => {
+	trapFocus(nav);
+});
